d11bb0337d4869a41a9fbe7528bed7426d23098f
Dilanka Perera <dilankaperera95@gmail.com>
2017-12-04 17:33:35 +0530

    Add new files

---
+++ header.php
@@ -0,0 +1,35 @@
+<!--Top header-->
+<div class="header-top">\
+	<div class="topword">
+		<ul>
+			<li><a href="#"> Create Account </a></li>
+			<li><a href="#">Login</a></li>		
+		</ul>	
+	</div>
+	<div class="view-cart-main">
+
+		<a href="#"><button class="view-cart"><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></button></a>
+		
+	</div>
+</div>
+<!--//Top header-->
+
+<!--Botttom header-->
+<div class="header_bottom">
+	<div class="container">
+		<a href="index.php"><img src="images/logo.png" id="logo"></a>
+			<div class="search_bar">
+				<form method="get" action="#" enctype="multipart/form-data">
+					<input type="search" name="Search" placeholder="Search..." required="">
+					<!-- <input type="submit" name="Search" value="Search"> -->
+					<button type="submit" class="btn btn-default search" aria-label="Left Align">
+					<i class="fa fa-search" aria-hidden="true"> </i>
+					</button>
+			
+
+				</form>
+		
+			</div>
+	</div>
+</div>
+<!--//Botttom header-->
---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Gzone Technologies</title>
+
+
+	<link rel="stylesheet" type="text/css" href="css/styles.css" media="all"/>
+	<link rel="stylesheet" href="css/bootstrap.css"/>
+	<link href="css/font-awesome.css" rel="stylesheet">
+
+</head>
+<body>
+
+	<!-- header -->
+	<?php include_once('header.php'); ?>
+
+	<!-- content -->
+	
+
+	<div class="main_content">
+		<!-- sidebar -->
+		<?php include_once('sidebar.php'); ?>
+		
+
+		<div class="right_content"></div>
+	
+</div>
+
+
+
+
+
+
+
+
+
+
+	<!-- footer -->
+	<?php include_once('footer.php'); ?>
+
+</body>
+</html>
