f87aafb3f3f0806c38e0d5d8484b6ae262c47af0
Unknown <kloransuhewa@gmail.com>
2017-12-04 18:56:32 +0530

    select user created

---
+++ select_user.php
@@ -0,0 +1,29 @@
+<html>
+
+<head>
+<title> select user </title>
+
+	<link rel="stylesheet" type="text/css" href="css/styles.css" media="all"/>
+</head>
+
+<body>
+  <div>
+
+
+
+  <div class="form">
+  <button type="submit" name="regAgent" class="button button-block"/> <b> Register as a Agent </b> </button>
+  </div>
+
+  <div class="form">
+  <button type="submit" name="regCustomer" class="button button-block"/> <b> Register as a Customer </b> </button>
+  </div>
+
+
+
+
+
+</div>
+
+</body>
+</html>
