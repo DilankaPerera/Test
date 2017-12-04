d6856de492ecf504530a962a39228578b8cf8b2c
Unknown <kloransuhewa@gmail.com>
2017-12-04 19:12:30 +0530

    divide buttons horizontally

--- select_user.php
+++ select_user.php
@@ -11,11 +11,11 @@
 
 
 
-  <div class="form">
+  <div class="form" id="formAlign_agent">
   <button type="submit" name="regAgent" class="button button-block"/> <b> Register as a Agent </b> </button>
   </div>
 
-  <div class="form">
+  <div class="form" id="formAlign_customer">
   <button type="submit" name="regCustomer" class="button button-block"/> <b> Register as a Customer </b> </button>
   </div>
 
