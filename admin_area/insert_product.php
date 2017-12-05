0694a4f5c20479b07a59bee5f545af0e39563163
Dilanka Perera <dilankaperera95@gmail.com>
2017-12-05 21:43:00 +0530

    Changes done

---
+++ admin_area/insert_product.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+	<title>Insert Products</title>
+</head>
+<body>
+
+	<form action="insert_product.php" method="post" enctype="multipart/form-data"  >
+
+		<table align="center" width="700" border="2" border-style="solid">
+
+			<tr align="center">
+				<td colspan="6"><h2>Insert New Product</h2></td>
+			</tr>
+
+			<tr>
+				<td>Product Name</td>
+				<td><input type="text" name="product_name"></td>
+			</tr>
+			
+
+
+
+
+
+
+
+		</table>
+
+	</form>
+
+
+</body>
+</html>
