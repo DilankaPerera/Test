d9b49470dc1444462f1da0f6c1f515c57ec3ee59
Unknown <kloransuhewa@gmail.com>
2017-12-06 22:14:05 +0530

    nav bar-updated

--- navigation_bar.php
+++ navigation_bar.php
@@ -1,79 +1,57 @@
-	<?php include_once('Templates/header.php'); ?>
+<?php include_once('Templates/header.php'); ?>
 
-
-  <html>
-  <head>
-    <link rel="stylesheet" type="text/css" href="css/styles.css" media="all"/>
-    <link href="css/font-awesome.css" rel="stylesheet">
-  </head>
-  <body>
-
+<html>
+<head>
+  <title> Navigation Bar </title>
+  <link rel="stylesheet" type="text/css" href="styles/style.css" media="all"/>
+  <link href="css/font-awesome.css" rel="stylesheet">
+</head>
+<body>
   <div class="navbar">
     <ul>
-      <li><a href="#home">Home</a></li>
-      <li><a href="#news">News</a></li>
-      <li><a href="#electronics">Electronics</a>
-<div class="dropdown">
-<ul>
-  <li><a href="#">Cell phones and Accessories</a></li>
- <li><a href="#" >Cameras and Photos</a></li>
-  <li><a href="#">Computer and Tablets</a></li>
-  <li><a href="#">TV and Audio</a></li>
-
-</ul>
-
-</div>
-  </li>
-      <li><a href="#fashion">Fashion</a>
+      <li><a href="#home"><i class="fa fa-home"></i> Home </a></li>
+      <li><a href="#news"><i class="fa fa-newspaper-o"></i> News</a></li>
+      <li><a href="#electronics"><i class="fa fa-plug"></i> Electronics</a>
         <div class="dropdown">
-        <ul>
-          <li><a href="#">Men's</a></li>
-         <li><a href="#" >Women's</a></li>
-          <li><a href="#">Kid's</a></li>
-
-
-        </ul>
-
+          <ul>
+            <li><a href="#">Cell phones and Accessories</a></li>
+            <li><a href="#" >Cameras and Photos</a></li>
+            <li><a href="#">Computer and Tablets</a></li>
+            <li><a href="#">TV and Audio</a></li>
+          </ul>
         </div>
-
-
-
       </li>
-      <li><a href="#sports">Sports Goods</a>
-      <div class="dropdown" >
-      <ul >
-                        <li><a href="#">Cricket</a></li>
-                       <li><a href="#" >Football</a></li>
-                        <li><a href="#">Rugger</a></li>
-                        <li><a href="#">Basketball</a></li>
-
-                    </ul>
-
-                  </div>
-                  </li>
-      <li><a href="#motors">Motors</a>
+      <li><a href="#fashion"><i class="fa fa-briefcase"></i> Fashion</a>
         <div class="dropdown">
-        <ul>
-          <li><a href="#">Cars & Vans</a></li>
-         <li><a href="#" >Motocycles</a></li>
-          <li><a href="#">Parts and Accessories</a></li>
-        
-
-        </ul>
-
+          <ul>
+            <li><a href="#">Men's</a></li>
+            <li><a href="#" >Women's</a></li>
+            <li><a href="#">Kid's</a></li>
+          </ul>
         </div>
-
-
-
-
       </li>
-
+      <li><a href="#sports"><i class="fa fa-futbol-o"></i> Sports Goods</a>
+        <div class="dropdown">
+          <ul >
+            <li><a href="#">Cricket</a></li>
+            <li><a href="#" >Football</a></li>
+            <li><a href="#">Rugger</a></li>
+            <li><a href="#">Basketball</a></li>
+          </ul>
+        </div>
+      </li>
+      <li><a href="#motors"><i class="fa fa-car"></i> Motors</a>
+        <div class="dropdown">
+          <ul>
+            <li><a href="#">Cars & Vans</a></li>
+            <li><a href="#" >Motocycles</a></li>
+            <li><a href="#">Parts and Accessories</a></li>
+          </ul>
+        </div>
+      </li>
     </ul>
   </div>
-
-  </body>
-  </html>
-
-
+</body>
+</html>
 
 <?php include_once('Templates/footer.php'); ?>
--- select_user.php
+++ select_user.php
@@ -1,32 +1,20 @@
 <?php include_once('Templates/header.php'); ?>
 
 <html>
-
 <head>
 <title> select user </title>
-
-	<link rel="stylesheet" type="text/css" href="css/styles.css" media="all"/>
+<link rel="stylesheet" type="text/css" href="styles/style.css" media="all"/>
+<link href="css/font-awesome.css" rel="stylesheet">
 </head>
-
 <body>
-  <div>
-
-
-
-  <div class="form" id="formAlign_agent">
-  <button type="submit" name="regAgent" class="button button-block"/> <b> Register as a Agent </b> </button>
-  </div>
-
-  <div class="form" id="formAlign_customer">
-  <button type="submit" name="regCustomer" class="button button-block"/> <b> Register as a Customer </b> </button>
-  </div>
-
-
-
-
-
-</div>
-
+	<div>
+		<div class="form" id="formAlign_agent">
+			<button type="submit" name="regAgent" class="button button-block"/> <b> Register as a Agent </b> </button>
+		</div>
+		<div class="form" id="formAlign_customer">
+			<button type="submit" name="regCustomer" class="button button-block"/> <b> Register as a Customer </b> </button>
+		</div>
+	</div>
 
 </body>
 </html>
