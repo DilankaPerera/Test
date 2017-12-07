9c4ac3efce04c1ce3b77d9fae68d95fbdeb05d28
Unknown <kloransuhewa@gmail.com>
2017-12-07 08:33:35 +0530

    forms updated

--- login.php
+++ login.php
@@ -1,41 +1,27 @@
 <?php include_once('Templates/header.php'); ?>
-
 <html>
 
 <head>
-<title> Login </title>
-
-<link rel="stylesheet" type="text/css" href="styles/style.css" media="all"/>
-<link href="css/font-awesome.css" rel="stylesheet">
-
+  <title> Login </title>
+  <link rel="stylesheet" type="text/css" href="styles/style.css" media="all"/>
+  <link href="css/font-awesome.css" rel="stylesheet">
 </head>
 
 <body>
-
-<div>
-  <form class="form">
-    <fieldset>
-      <legend> <b> User Login </b> </legend>
-
-      <div>
-        <table>
-          <tr>
-            <td> <label for="username"> Username : </label> </td>
-            <td> <input type="text" size="25" id="username"> </td>
-          </tr>
-          <tr>
-            <td><label for="password"> Password : </label> </td>
-            <td><input type="password" size="25" id="paasword"></td>
-          </tr>
-        </table>
-      </div>
-      <input type="submit"  value="Login">
-    </fieldset>
-  </form>
-</div>
-
-
+  <div class="reg_form_grid">
+    <h3 class="heading_reg">Log in</h3>
+    <ul>
+      <li> <label for="username"> Username  </label> </li>
+      <li> <input type="text" size="50" id="username"> </li>
+      <br>
+      <li><label for="password"> Password  </label> </li>
+      <li><input type="password" size="50" id="paasword"></li>
+    </ul>
+    <br>
+    <input type="submit"  value="Login">
+  </div>
 </body>
+
 </html>
 
 <?php include_once('Templates/footer.php'); ?>
