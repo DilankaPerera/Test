6018280cfe1692f22df98abc0ff1d0a76d3d14c1
Unknown <kloransuhewa@gmail.com>
2017-12-11 13:11:13 +0530

    Auto stash before merge of "master" and "origin/master"
    new navbar created

--- Templates/header.php
+++ Templates/header.php
@@ -77,6 +77,49 @@ $conn = mysqli_connect($hostname,$username,$password,$database);
 </div>
 <!--//Botttom header-->
 
-<div class="menu">
-	
+<!-- <div class="menu"> -->
+
+  <div class="hbnavbar">
+	  <a href="#home">Home</a>
+	  <a href="#news">All Products</a>
+
+		<div class="hbdropdown">
+	    <button class="hbdropbtn">Mobile & Tablets
+	      <i class="fa fa-caret-down"></i>
+	    </button>
+	    <div class="hbdropdown-content">
+	      <a href="#">Mobile & Tablets</a>
+	      <a href="#">Mobile & Tablets Accessories</a>
+	      <a href="#">Other Smart Devices</a>
+	    </div>
+	  </div>
+
+		<div class="hbdropdown">
+			<button class="hbdropbtn">Computers & Laptops
+				<i class="fa fa-caret-down"></i>
+			</button>
+			<div class="hbdropdown-content">
+				<a href="#">Computer Accessories</a>
+				<a href="#">Datacards & Routers</a>
+				<a href="#">Storage</a>
+				<a href="#">Gaming</a>
+			</div>
+		</div>
+
+		<div class="hbdropdown">
+			<button class="hbdropbtn">Electronics
+				<i class="fa fa-caret-down"></i>
+			</button>
+			<div class="hbdropdown-content">
+				<a href="#">Camera</a>
+				<a href="#">Audio & Video Accessories</a>
+				<a href="#">Speakers</a>
+				<a href="#">Headphones</a>
+				<a href="#">Personal Care Applications</a>
+			</div>
+		</div>
+
+		<a href="#news">Other Products</a>
+
+
 </div>
